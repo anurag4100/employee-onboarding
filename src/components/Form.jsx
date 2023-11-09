fa98d70e8d5a501946ccffb22e608f5cf43992a3
anurag4100 <anuragbharatpandey@gmail.com>
2023-11-09 18:09:41 +0530

    modal and spinner

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useFormik } from 'formik';
 import {
   Box,
@@ -7,7 +7,13 @@ import {
   StepLabel,
   Grid,
   FormHelperText,
-  Button
+  Button,
+  CircularProgress,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
 } from '@mui/material';
 import PersonalInfo from './PersonalInfo';
 import AccountDetails from './AccountDetails';
@@ -17,9 +23,17 @@ const steps = [' Personal Info', 'Documents', 'e-Sign and Submit'];
 
 const Form = () => {
   const [activeStep, setActiveStep] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [successDialogOpen, setSuccessDialogOpen] = useState(false);
+  const [failureDialogOpen, setFailureDialogOpen] = useState(false);
 
   const handleBack = () => {
     setActiveStep((prevStep) => prevStep - 1);
+  };
+
+  const handleCloseDialogs = () => {
+    setSuccessDialogOpen(false);
+    setFailureDialogOpen(false);
   };
 
   const formik = useFormik({
@@ -30,62 +44,73 @@ const Form = () => {
       firstName: '',
       lastName: '',
       phone: '',
-      residence: ''
+      residence: '',
     },
     
     onSubmit: async (values) => {
-      if (activeStep === steps.length - 1) {
-        console.log('last step');
-        console.log(values);
-        try {
-        // Get authentication token
-      const tokenResponse = await fetch('https://realm.mongodb.com/api/client/v2.0/app/data-jttnm/auth/providers/local-userpass/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          username: 'ServiceAccount1',
-          password: 'nnEVXPvFsrHeKFob',
-        }),
-      });
-      if (!tokenResponse.ok) {
-        console.error('Failed to get authentication token:', tokenResponse);
-        return;
-      }
-      const { access_token } = await tokenResponse.json();
-      
-      // Submit data to MongoDB with the obtained token
-      const response = await fetch('https://ap-south-1.aws.data.mongodb-api.com/app/data-jttnm/endpoint/data/v1/action/insertOne', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${access_token}`, // Pass the obtained token in the Authorization header
-        },
-        body: JSON.stringify({
-          collection: 'EmpOnboardingInfo',
-          database: 'EmpOnboarding',
-          dataSource: 'EmployeeInfoTest',
-          document: values,
-        }),
-      });
+      try {
+        setLoading(true);
 
-      if (response.ok) {
-        console.log('Data successfully submitted to MongoDB:', response);
-      } else {
-        console.error('Failed to submit data to MongoDB:', response);
+        if (activeStep === steps.length - 1) {
+          console.log('last step');
+          console.log(values);
+
+          // Get authentication token
+          const tokenResponse = await fetch('https://realm.mongodb.com/api/client/v2.0/app/data-jttnm/auth/providers/local-userpass/login', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              username: 'ServiceAccount1',
+              password: 'nnEVXPvFsrHeKFob',
+            }),
+          });
+
+          if (!tokenResponse.ok) {
+            console.error('Failed to get authentication token:', tokenResponse);
+            setFailureDialogOpen(true);
+            return;
+          }
+
+          const { access_token } = await tokenResponse.json();
+          
+          // Submit data to MongoDB with the obtained token
+          const response = await fetch('https://ap-south-1.aws.data.mongodb-api.com/app/data-jttnm/endpoint/data/v1/action/insertOne', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${access_token}`, // Pass the obtained token in the Authorization header
+            },
+            body: JSON.stringify({
+              collection: 'EmpOnboardingInfo',
+              database: 'EmpOnboarding',
+              dataSource: 'EmployeeInfoTest',
+              document: values,
+            }),
+          });
+
+          if (response.ok) {
+            console.log('Data successfully submitted to MongoDB:', response);
+            setSuccessDialogOpen(true);
+          } else {
+            console.error('Failed to submit data to MongoDB:', response);
+            setFailureDialogOpen(true);
+          }
+        } else {
+          setActiveStep((prevStep) => prevStep + 1);
+        }
+      } catch (error) {
+        console.error('Error during form submission:', error);
+        setFailureDialogOpen(true);
+      } finally {
+        setLoading(false);
       }
-    } catch (error) {
-      console.error('Error during form submission:', error);
-    }
-      } else {
-        setActiveStep((prevStep) => prevStep + 1);
-      }
-    }
+    },
   });
 
   const formContent = (step) => {
-    switch(step) {
+    switch (step) {
       case 0:
         return <AccountDetails formik={formik} />;
       case 1:
@@ -93,21 +118,38 @@ const Form = () => {
       case 2:
         return <ReviewInfo formik={formik} />;
       default:
-        return <div>404: Not Found</div>
+        return <div>404: Not Found</div>;
     }
   };
 
   return (
     <Box
       sx={{
+        position: 'relative',
         maxWidth: '600px',
-        padding: 2
+        padding: 2,
       }}
     >
-      <Stepper
-        activeStep={activeStep}
-        orientation="horizontal"
-      >
+      {/* Loading Spinner */}
+      {loading && (
+        <Box
+          sx={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            background: 'rgba(255, 255, 255, 0.8)', // Semi-transparent white background
+          }}
+        >
+          <CircularProgress color="primary" />
+        </Box>
+      )}
+
+      <Stepper activeStep={activeStep} orientation="horizontal">
         {steps.map((label, index) => (
           <Step key={index}>
             <StepLabel>{label}</StepLabel>
@@ -115,31 +157,16 @@ const Form = () => {
         ))}
       </Stepper>
       <Grid container>
-        <Grid
-          item
-          xs={12}
-          sx={{ padding: '20px' }}
-        >
+        <Grid item xs={12} sx={{ padding: '20px' }}>
           {formContent(activeStep)}
         </Grid>
         {formik.errors.submit && (
-          <Grid
-            item
-            xs={12}
-          >
-            <FormHelperText error>
-              {formik.errors.submit}
-            </FormHelperText>
+          <Grid item xs={12}>
+            <FormHelperText error>{formik.errors.submit}</FormHelperText>
           </Grid>
         )}
-        <Grid
-          item
-          xs={12}
-        >
-          <Button
-            disabled={activeStep === 0}
-            onClick={handleBack}
-          >
+        <Grid item xs={12}>
+          <Button disabled={activeStep === 0} onClick={handleBack}>
             Back
           </Button>
           {activeStep === steps.length - 1 ? (
@@ -150,11 +177,37 @@ const Form = () => {
             <Button onClick={formik.handleSubmit}>
               Next
             </Button>
-          ) }
+          )}
         </Grid>
       </Grid>
+
+      {/* Success Dialog */}
+      <Dialog open={successDialogOpen} onClose={handleCloseDialogs}>
+        <DialogTitle>Success</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Data successfully submitted to MongoDB.</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialogs} color="primary">
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      {/* Failure Dialog */}
+      <Dialog open={failureDialogOpen} onClose={handleCloseDialogs}>
+        <DialogTitle>Error</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Failed to submit data to MongoDB.</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialogs} color="primary">
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Box>
-  )
-}
+  );
+};
 
 export default Form;
