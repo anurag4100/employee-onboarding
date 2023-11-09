328edc865cb5e2a1397f34a2570f99bd20ca3912
anurag4100 <anuragbharatpandey@gmail.com>
2023-11-09 18:26:49 +0530

    documents

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,82 +1,82 @@
+import React from 'react';
 import {
-    TextField,
-    Grid
-  } from '@mui/material';
-  
-  const PersonalInfo = (props) => {
-    const { formik } = props;
-    return (
-      <Grid
-        container
-        spacing={2}
-      >
-        <Grid
-          item
-          xs={6}
-        >
-          <TextField
-            name="firstName"
-            label="First Name"
-            variant="outlined"
-            size='small'
-            fullWidth
-            value={formik.values.firstName}
-            onChange={formik.handleChange}
-            error={formik.touched.firstName && Boolean(formik.errors.firstName)}
-            helperText={formik.touched.firstName && formik.errors.firstName}
-          />
-        </Grid>
-        <Grid
-          item
-          xs={6}
-        >
-          <TextField
-            name="lastName"
-            label="Last Name"
-            variant="outlined"
-            size="small"
-            fullWidth
-            value={formik.values.lastName}
-            onChange={formik.handleChange}
-            error={formik.touched.lastName && Boolean(formik.errors.lastNamel)}
-            helperText={formik.touched.lastName && formik.errors.lastName}
-          />
-        </Grid>
-        <Grid
-          item
-          xs={12}
-        >
-          <TextField
-            name="phone"
-            label="Phone Number"
-            variant="outlined"
-            type="phone"
-            fullWidth
-            size="small"
-            value={formik.values.phone}
-            onChange={formik.handleChange}
-            error={formik.touched.phone && Boolean(formik.errors.phone)}
-            helperText={formik.touched.phone && formik.errors.phone}
-          />
-        </Grid>
-        <Grid
-          item
-          xs={12}
-        >
-          <TextField
-            name="residence"
-            label="Residence"
-            variant="outlined"
-            size="small"
-            fullWidth
-            value={formik.values.residence}
-            onChange={formik.handleChange}
-            error={formik.touched.residence && Boolean(formik.errors.residence)}
-            helperText={formik.touched.residence && formik.errors.residence}
-          />
-        </Grid>
+  TextField,
+  Grid,
+  Button,
+  Input,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from '@mui/material';
+
+const documentFields = [
+  { name: 'PAN Card', fieldName: 'panCard' },
+  { name: 'Aadhar Card', fieldName: 'aadharCard' },
+  { name: 'Other Document', fieldName: 'otherDocument' },
+];
+
+const PersonalInfo = (props) => {
+  const { formik } = props;
+
+  const handleFileChange = (fieldName, event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        formik.setFieldValue(fieldName, reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <Grid container spacing={2}>
+      
+      {/* File Upload Fields in Tabular View */}
+      <Grid item xs={12}>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Document</TableCell>
+                <TableCell>Upload</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {documentFields.map((document) => (
+                <TableRow key={document.fieldName}>
+                  <TableCell>{document.name}</TableCell>
+                  <TableCell>
+                    <Input
+                      type="file"
+                      accept="image/*,application/pdf"
+                      onChange={(e) => handleFileChange(document.fieldName, e)}
+                    />
+                    {formik.values[document.fieldName] && (
+                      <Button
+                        variant="outlined"
+                        color="primary"
+                        onClick={() => {
+                          // Handle e-sign for the document
+                          console.log(`E-Sign for ${document.name}`);
+                        }}
+                      >
+                        E-Sign
+                      </Button>
+                    )}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Grid>
-    )
-  }
-  
-  export default PersonalInfo
+    </Grid>
+  );
+};
+
+export default PersonalInfo;
